ab72b404f0bf8593668d44cfaba7b3ac208e02a7
dimadudin <dmitriy.i.dudin@gmail.com>
2024-02-29 14:17:40 +0300

    feat: read cmd line args

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('henlo wurld')
+const { argv } = require('node:process');
+
+function main() {
+    if (argv.length < 3) {
+        console.log('not enough arguments supplied');
+    } else if (argv.length > 3) {
+        console.log('too many arguments supplied');
+    } else {
+        console.log(`Initiating __C.R.A.W.L.__ at ${argv[2]}`)
+    }
+}
+
+main()
