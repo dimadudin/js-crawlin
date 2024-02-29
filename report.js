314360b6f565d5b234b5e2d020603f0196cab8b4
dimadudin <dmitriy.i.dudin@gmail.com>
2024-02-29 15:52:49 +0300

    fix: report now prints in descending order

--- report.js
+++ report.js
@@ -1,10 +1,7 @@
 function printReport(pages) {
     console.log('Initiating report');
-    const sortableLinks = []
-    for (const url in pages) {
-        sortableLinks.push([url, pages[url]])
-    }
-    sortableLinks.sort((a, b) => (a[1] - b[1]));
+    const sortableLinks = Object.entries(pages)
+    sortableLinks.sort((a, b) => (b[1] - a[1]));
     sortableLinks.forEach((a) => { console.log(`Found ${a[1]} internal links to ${a[0]}`) })
 }
 
